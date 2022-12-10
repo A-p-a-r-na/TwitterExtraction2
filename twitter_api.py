41f968032ffb2c0b2e3553d5b83b810b44af18d9
APARNA <aparnalekshmi23@gmail.com>
2022-12-10 16:49:46 +0530

    Update twitter_api.py

--- twitter_api.py
+++ twitter_api.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = "6iLoRCO2rgatvYVK9sRemHeqd"
-consumer_secret = "qIjZGEjUaJ2p0ULuxCaemaOnwh4AkUhkCC2aZwc3rQ4FU4J1CH"
-access_key = "1592966656011083776-FirtW5HCmlN30d5KuuT72EqIGkY5C6"
-access_secret = "O1uOPGRlZ2Mqa7nuq6xGpFMISMy7cXYwIUEYXbYkEw3mg"
+consumer_key = " "
+consumer_secret = " "
+access_key = " "
+access_secret = " "
 
 # Function to extract tweets
 def get_tweets(username):
